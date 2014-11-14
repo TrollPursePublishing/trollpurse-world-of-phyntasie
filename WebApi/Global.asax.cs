b2f4bf1290f7afd92cd0b0d025f97563f8ca22cd
hollsteinm <hollsteinm@hotmail.com>
2014-11-14 16:31:12 -0600

    Updating for code first migrations as well

--- WebApi/Global.asax.cs
+++ WebApi/Global.asax.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -15,13 +16,26 @@ namespace WebApi
     {
         protected void Application_Start()
         {
+            //Various third party configurations
             JsonConvert.DefaultSettings = () => new JsonSerializerSettings
             {
                 ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             };
 
+            //Migrations
+            var authmigration = new WebApi.Migrations.Configuration();
+            var migrator = new DbMigrator(authmigration);
+            migrator.Update();
+
+            var gamemigration = new AdventureQuestGame.Migrations.Configuration();
+            migrator = new DbMigrator(gamemigration);
+            migrator.Update();
+
+            //Initiailizers
             Database.SetInitializer(new GameInit());
             Database.SetInitializer(new AuthInit());
+
+            //Web Configurations
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
