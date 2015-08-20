044141b004f487868a63fd4d2149572b72051264
hollsteinm <hollsteinm@hotmail.com>
2015-08-20 15:44:14 -0500

    dunno

---
+++ AdventureQuestGame/Models/Riddle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureQuestGame.Models
+{
+    public class Riddle
+    {
+        public Riddle(string question, string answer)
+        {
+            Question = question;
+            Answer = answer;
+            Id = Guid.NewGuid();
+        }
+
+        protected Riddle() { }
+
+        public bool IsAnswer(string input)
+        {
+            return input.ToLower().Contains(Answer.ToLower());
+        }
+
+        public Guid Id { get; private set; }
+        public string Question { get; protected set; }
+        public string Answer { get; protected set; }
+    }
+}
