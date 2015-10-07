2d22da55ee6a21a7d78fcca3dd2954019add0694
hollsteinm <hollsteinm@hotmail.com>
2015-10-07 13:35:42 -0500

    Email notifications

--- WebApi.Backend.Admin/Controllers/EventAdminController.cs
+++ WebApi.Backend.Admin/Controllers/EventAdminController.cs
@@ -1,10 +1,26 @@
-﻿using AdventureQuestGame.Admin;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using WebApi;
+using Newtonsoft.Json;
+using WebApi.Models;
+using AdventureQuestGame.Services;
+using System.Web.Http.Description;
+using System.Text;
+using System.Linq;
+using AdventureQuestGame.Admin;
 using WebApi.Backend.Admin.Models;
 
 namespace WebApi.Backend.Admin.Controllers
@@ -14,6 +30,19 @@ namespace WebApi.Backend.Admin.Controllers
     public class EventAdminController : ApiController
     {
         EventAdministration service = new EventAdministration();
+
+        private ApplicationUserManager _userManager = null;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
 
         [HttpGet]
         [Route("GetEvents")]
@@ -26,7 +55,28 @@ namespace WebApi.Backend.Admin.Controllers
         [Route("CreateEvent")]
         public IHttpActionResult CreateEvent([FromBody]EventViewModel model)
         {
-            return Ok(service.CreateEvent(model.Title, model.Description));
+            try
+            {
+                var result = service.CreateEvent(model.Title, model.Description);
+                Task.Run(async () =>
+                {
+                    foreach(var u in UserManager.Users.Where(u => u.EmailConfirmed &&
+                        u.SendMail &&
+                        !String.IsNullOrEmpty(u.Email)))
+                    {
+                        await UserManager.SendEmailAsync(u.Id,
+                            String.Format("AdventureGameQuest: {0}", model.Title),
+                            String.Format("{0}\r\n<br /> Haven't played in a while? <a href=\"{1}\">Press to Play!</a><br />\r\n<br /> Don't want to receive these emails anymore? Log in to your account and disable Email Notifications.",
+                                model.Description,
+                               @"http://adventuregamequest.azurewebsites.net"));
+                    }
+                });
+                return Ok(result);
+            }
+            catch(Exception)
+            {
+                return InternalServerError();
+            }
         }
     }
 }
